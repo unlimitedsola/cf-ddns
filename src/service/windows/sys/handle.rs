de4d724209a8ad3d3b0f9fd5ae1f6501bc3ecb4b
Sola <dev@sola.love>
2024-07-13 06:36:31 +0800

    fix: compile error

--- src/service/windows/sys/handle.rs
+++ src/service/windows/sys/handle.rs
@@ -1,24 +1,24 @@
-use windows::Win32::Security;
 use windows::Win32::System::Services;
+use windows::Win32::System::Services::SC_HANDLE;
 
-/// A handle wrapper for holding underlying [Security::SC_HANDLE].
+/// A handle wrapper for holding underlying [SC_HANDLE].
 ///
 /// [ScHandle] implements [Drop] for automatically closing unused handles.
-/// However, incorrect implementation may cause the underlying handle gets
+/// However, incorrect implementation may cause the underlying handle to get
 /// invalidated before being dropped, or still holding the underlying handle
 /// after this wrapper gets dropped.
-pub struct ScHandle(Security::SC_HANDLE);
+pub struct ScHandle(SC_HANDLE);
 
 impl ScHandle {
-    /// Creates a wrapper for underlying [Security::SC_HANDLE].
-    /// Make sure to check [Security::SC_HANDLE::is_invalid] before calling.
-    pub unsafe fn new(handle: Security::SC_HANDLE) -> Self {
+    /// Creates a wrapper for underlying [SC_HANDLE].
+    /// Make sure to check [SC_HANDLE::is_invalid] before calling.
+    pub unsafe fn new(handle: SC_HANDLE) -> Self {
         ScHandle(handle)
     }
 
-    /// Returns the underlying [Security::SC_HANDLE].
+    /// Returns the underlying [SC_HANDLE].
     /// For safety reasons, the returned handle SHOULD NOT be kept alive.
-    pub fn raw_handle(&self) -> Security::SC_HANDLE {
+    pub fn raw_handle(&self) -> SC_HANDLE {
         self.0
     }
 }
